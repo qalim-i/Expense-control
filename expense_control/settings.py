b7935b5040620273ad6a1f13d6f28c915393cd81
qalim_i <132349976+qalim-i@users.noreply.github.com>
2026-01-12 20:46:33 +0530

    3rd deploy commit

--- expense_control/settings.py
+++ expense_control/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-ul%a9!c@uakh)26+&_v1v@#zh67&!&1ktl@q_&ktyqr6htzb$r"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1', 'https://expense-control-gamma.vercel.app/']
 
 
 # Application definition
